4e2bd74f8e7b40931081b38aea6a91861a598e1b
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-27 02:49:47 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,51 +1,7 @@
 def first_challenge
-   epic_tragedy = {
-     :montague => {
-       :patriarch => {
-         :name => "Lord Montague",
-         :age => "53"
-       },
-       :matriarch => {
-         :name => "Lady Montague",
-         :age => "54"
-       },
-       :hero => {
-         :name => "Romeo",
-         :age => "15"
-       },
-       :hero_friends => [{
-         :name => "Benvolio",
-         :age => "17",
-         :attitude => "hot-headed"
-       }
-       ]
-       
-     },
-     :capulet => {
-       :patriarch => {
-         :name => "Lord Capulet",
-         :age => "50"
-       },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "50"
-      },
-      :heroine => {
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
-      },
-      :heroine_friends => [{
-        :name => "Steven",
-        :age => "30",
-        :attitude => "confused"
-      },
-      {
-        :name => "Nurse",
-        :age => "44",
-        :attitude => "worried"
-      }
-     ]
-     } 
-   }
+  epic_tragedy = {}
+  epic_tragedy = {
+    :montague => {},
+    :capulet => {}
+  }
 end
